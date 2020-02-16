0a15769b6f6355b18c789c74daef8b078d99f293
@mauroebordon <mauro.e.bordon@gmail.com>
2020-02-16 01:47:01 -0300

    getting used to Haxe and Heaps

--- src/Game.hx
+++ src/Game.hx
@@ -1,29 +1,18 @@
 import Data;
 
 class Game extends hxd.App {
-	var square: h2d.Bitmap;
+
 	var fps: h2d.Text;
 	var worm: Worm;
-	var head: h3d.Vector;
-	var tail: h3d.Vector;
 	
 	override function init(){
-		var head = new h3d.Vector(150, 250);
-		var tail = new h3d.Vector(300, 550);
-		worm = new Worm(head, tail);
-		var tile= h2d.Tile.fromColor(0x44FF90, 100, 100);
 		fps = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-		square = new h2d.Bitmap(tile, s2d);
-		worm = new Worm(head, tail);
-		square.x = s2d.width * 0.5;
-		square.y = s2d.height * 0.5;
-		square.tile.dx = -50;
-		square.tile.dy = -50;
+		worm = new Worm(100, 100, 300, 300);
+		worm.stroke();
 	}
 	override function update(dt: Float){
-		square.rotation += Math.PI * dt;
 		fps.text = Std.string(Math.ceil(1/dt));
-		worm.draw();
+		worm.update();
 	}
 
 	public static var inst : Game;
--- src/Worm.hx
+++ src/Worm.hx
@@ -1,56 +1,49 @@
-class Worm {
+import h2d.Bitmap;
+
+class Worm extends h2d.Object{
 	var game : Game;
-	public var head: h3d.Vector;
-	public var tail: h3d.Vector;
+	public var head: h2d.Graphics;
+	public var tail: h2d.Graphics;
 	public var velocity: h3d.Vector;
 	public var acceleration: h3d.Vector;
 	public var reach: Float;
 	public var dna: DNA;
 	public var fitness: Float;
 
-	public var width = 100;
-	public var height = 30;
-
-	var body: h2d.Graphics;
-	var p1: h2d.Graphics;
-	var p2: h2d.Graphics;
-
-	public function new(head: h3d.Vector, tail: h3d.Vector){
+	public var width: Int;
+	public var height: Int;
+	public var size: Float;
+	
+	public function new(hx=450, hy=450, tx=550, ty=550, wid=100, hei=30){
+		super();
 		game = Game.inst;
-		this.head = head;
-		this.tail = tail;
+		head = new h2d.Graphics(game.s2d);
+		tail = new h2d.Graphics(game.s2d);
+		head.x = hx;
+		head.y = hy;
+		tail.x = tx;
+		tail.y = ty;
+		width = wid;
+		height = hei;
+		x = Math.floor((head.x + tail.x)/2);
+		y = Math.floor((head.y + tail.y)/2);
+		rotation = Math.atan2(head.y - tail.y, head.x - tail.x);
 		velocity = new h3d.Vector();
 		acceleration = new h3d.Vector();
-		reach = 20;
-		body = new h2d.Graphics(game.s2d);
-		p1 = new h2d.Graphics(game.s2d);
-		p2 = new h2d.Graphics(game.s2d);
+		size = Math.sqrt(Math.pow(tail.x-head.x, 2) + Math.pow(tail.y-head.y,2))/2;
+
 	}
 
-	public function draw(){
-		var size = this.head.distance(this.tail)/2;
-		var k = 10;
-	
-		this.body.beginFill(0x2245DDFF);
-		for( i in 0...k+1 ) {
-		  var a = Math.PI * i / k - Math.PI / 2;
-		  this.body.lineTo(size + this.height * Math.cos(a), this.height * Math.sin(a));
-		}
-		for( i in 0...k+1 ) {
-		  var a = Math.PI * i / k + Math.PI / 2;
-		  this.body.lineTo(-size + this.height * Math.cos(a), this.height * Math.sin(a));
-		}
-		this.body.endFill();
-		this.body.x = this.body.tile.dx = Math.floor((this.head.x + this.tail.x)/2);
-		this.body.y = this.body.tile.dy =  Math.floor((this.head.y + this.tail.y)/2);
-		this.body.rotation = Math.atan2(this.head.y - this.tail.y, this.head.x - this.tail.x);
+	public function stroke(){
+		head.beginFill(0x2245DDFF);
+		head.drawCircle(0, 0, height);
+		head.endFill();
+		tail.beginFill(0x2245DDFF);
+		tail.drawCircle(0, 0, height);
+		tail.endFill();
+	}
 
-		this.p1.beginFill(0x00FF0000);
-		this.p1.drawRect(this.head.x-1, this.head.y-1, 3, 3);
-		this.p1.endFill();
-		this.p2.beginFill(0x00FF0000);
-		this.p2.drawRect(this.tail.x-1, this.tail.y-1, 3, 3);
-		this.p2.endFill();
-		
+	public function update() {
+
 	}
 }
